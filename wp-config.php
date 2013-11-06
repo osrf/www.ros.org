7ca97d3924d7729cbd138f4d0c18fb1ab6bddd23
Brian Gerkey <gerkey@osrfoundation.org>
2013-11-06 00:43:27 +0000

    modify wp-config.php to point to separated content

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,11 @@
  * @package WordPress
  */
 
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
