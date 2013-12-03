c4467e6194d1a315fa8d41d59e4c62f53cfef92e
Dirk Thomas <dthomas@osrfoundation.org>
2013-12-02 16:21:03 -0800

    Update commit-db.sh

--- commit-db.sh
+++ commit-db.sh
@@ -4,7 +4,7 @@ set -e
 dir=`mktemp -d`
 git clone -b wordpressdb git@github.com:osrf/www.ros.org.git $dir/www.ros.org
 cd $dir/www.ros.org
-mysqldump -u XXX -pXXX --skip-extended-insert wordpress > wordpress.sql
+mysqldump -h XXX -u XXX -pXXX --skip-extended-insert XXX > wordpress.sql
 git add wordpress.sql
 git commit -m "automatic db udpate"
 git push origin wordpressdb
